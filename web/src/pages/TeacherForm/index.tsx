b09e5b119fb08b057e0a58d3574269bd4b8766ec
Davi Lima <davi.n.lima99@gmail.com>
2020-08-06 16:41:51 -0300

    Add backend

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -6,24 +6,7 @@ import "./styles.css";
 function TeacherForm() {
     return (
         <div className="container" id="page-teacher-form">
-            <PageHeader title="Que incrível que você quer dar aulas.">
-                <form action="" id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject"/>
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia da semana</label>
-                        <input type="text" id="week_day"/>
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time"/>
-                    </div>
-                </form>
-            </PageHeader>   
+            <PageHeader title="Que incrível que você quer dar aulas." />
         </div>
     );
 }
